a24b12813f6577da74ed0f02839fbf5182e6514c
tomkarp <karpthomas@gmail.com>
2024-06-03 09:30:23 +0200

    Abschluss FA

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const pty = require('node-pty');
 const expressWs = require('express-ws')(app);
-
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-
 let DEBUG = true;
+const port = 3000
 
-// trust proxy uniquelocal
 app.set('trust proxy', 'uniquelocal')
 
 app.use(express.static(__dirname + '/web'));
@@ -18,35 +15,37 @@ app.use('/static/xterm', express.static(__dirname + '/../node_modules/xterm/lib'
 app.use('/static/xterm/css', express.static(__dirname + '/../node_modules/xterm/css'));
 app.use('/static/xterm-addon-fit', express.static(__dirname + '/../node_modules/xterm-addon-fit/lib'));
 app.use('/static/xterm-addon-attach', express.static(__dirname + '/../node_modules/xterm-addon-attach/lib'));
-app.use('/static/monaco-editor', express.static(__dirname + '/../node_modules/monaco-editor'))
+app.use('/static/monaco-editor', express.static(__dirname+'/../node_modules/monaco-editor'))
+app.use('/static/jquery-resizable-dom', express.static(__dirname+'/../node_modules/jquery-resizable-dom/dist'))
+app.use('/static/jquery-resizable-dom/assets', express.static(__dirname+'/../node_modules/jquery-resizable-dom/assets'))
+app.use('/static/jquery', express.static(__dirname+'/../node_modules/jquery/dist'))
 
 app.disable('x-powered-by')
 
-
-let terminals = {};
+let terminals = {}; 
 let logs = {};
 
 app.post('/terminals', (req, res) => {
     repl_name = uuidv4();
-    let cols = parseInt(req.query.cols),
-        rows = parseInt(req.query.rows);
+    cols = parseInt(req.query.cols),
+    rows = parseInt(req.query.rows);
+    
+    fs.writeFileSync('/tmp/'+repl_name, 'module Main exposing (..)\nmessage = "Hello World"');
 
-
-    fs.writeFileSync('/tmp/' + repl_name, 'module Main exposing (..)\nnachricht = "Hello World"');
-
-    term = pty.spawn('docker', ["run", "-it", "--rm", "-m", "256m", "--cpus", "0.25", "-v", "/tmp/" + repl_name + ":/src/Main.elm", "--name", repl_name, "elm-repl"], {
+    term = pty.spawn('docker', ["run", "-it", "--rm", "-m", "256m", "--cpus", "0.25", "-v", "/tmp/"+repl_name+":/src/Main.elm", "--name", repl_name, "elm-repl"], {
         name: 'xterm-color',
-        cols: cols,
-        rows: rows
+            cols: cols,
+            rows: rows
     });
-
-    console.log(`Created terminal with PID: ${term.pid}, container: ${repl_name}, size: ${cols}x${rows}`);
+    
+    if (DEBUG) console.log(`Created terminal with PID: ${term.pid}, container: ${repl_name}, size: ${cols}x${rows}`);
+    
     terminals[repl_name] = term;
-
+    
     term.on('data', (data) => {
         logs[repl_name] += data;
     });
-
+    
     res.send(repl_name);
     res.end();
     logs[repl_name] = ""
@@ -60,7 +59,6 @@ app.post('/terminals/:repl_name/size', (req, res) => {
         return;
     }
     let term = terminals[req.params.repl_name],
-        pid = parseInt(term.pid),
         cols = parseInt(req.query.cols),
         rows = parseInt(req.query.rows);
 
@@ -69,55 +67,55 @@ app.post('/terminals/:repl_name/size', (req, res) => {
     if (DEBUG) console.log('Resized terminal ' + req.params.repl_name + ' to ' + cols + ' cols and ' + rows + ' rows.');
 });
 
-app.post('/save/:repl_name', (req, res) => {
+app.post('/save/:repl_name', (req,res) => {
     if (!terminals[req.params.repl_name]) {
         res.end();
         if (DEBUG) console.log('saving: terminal not existing ' + req.params.repl_name);
         return;
     }
-    fs.writeFileSync('/tmp/' + req.params.repl_name, req.query.data);
+    fs.writeFileSync('/tmp/'+req.params.repl_name, req.query.data);
     res.end();
-    if (DEBUG) console.log("Saved file" + req.params.repl_name);
+    if (DEBUG) console.log("Saved file"+req.params.repl_name);
 })
 
 
 app.listen(port, () => {
-    console.log(`app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
 
 app.ws('/ws/:repl_name', (ws, req) => {
     let repl_name = req.params.repl_name,
         term = terminals[repl_name];
-    console.log('Connected to terminal ' + repl_name);
+    if (DEBUG) console.log('Connected to terminal ' + repl_name);
 
     try {
         ws.send(logs[repl_name]);
-    } catch (ex) {
-        console.error(ex)
+    } catch (e) {
+        console.error(e)
         ws.close();
     }
 
     term.on('data', (data) => {
         try {
             ws.send(data);
-        } catch (ex) {
-            // The WebSocket is not open, ignore
+        } catch (e) {
+            console.error(e)
         }
     });
 
     term.on('exit', (code, signal) => {
         try {
             ws.close();
-        } catch (ex) {
-            console.error(ex)
+        } catch (e) {
+            console.error(e)
         }
     });
 
     ws.on('message', (msg) => {
         try {
             term.write(msg);
-        } catch (ex) {
-            console.error(ex)
+        } catch (e) {
+            console.error(e)
             ws.close();
         }
     });
@@ -126,13 +124,13 @@ app.ws('/ws/:repl_name', (ws, req) => {
         try {
             let stop_container = pty.spawn('docker', ["rm", "-f", repl_name]);
             stop_container.on('close', code => {
-                console.log("Stopped container " + repl_name);
+                if (DEBUG) console.log("Stopped container "+repl_name);
             });
             delete terminals[repl_name];
             delete logs[repl_name];
-            fs.unlinkSync('/tmp/' + repl_name);
-        } catch (ex) {
-            console.error(ex)
+            fs.unlinkSync('/tmp/'+repl_name);
+        } catch (e) {
+            console.error(e)
         }
     });
 });
---
+++ src/web/script.js
@@ -0,0 +1,259 @@
+// EDITOR
+// compress and return compressed string as base64
+function compress(string, encoding) {
+    const byteArray = new TextEncoder().encode(string);
+    const cs = new CompressionStream(encoding);
+    const writer = cs.writable.getWriter();
+    writer.write(byteArray);
+    writer.close();
+    return new Response(cs.readable).arrayBuffer().then(function (arrayBuffer) {
+        return btoa(String.fromCharCode.apply(null, new Uint8Array(arrayBuffer)));
+    });
+}
+
+// take compressed base64 string as string and return decompressed string
+function decompress(string, encoding) {
+    try {
+        atob(string);
+    } catch (e) {
+        alert('Compressed data is not valid');
+        return;
+    }
+    const byteArray = Uint8Array.from(atob(string), c => c.charCodeAt(0));
+    const ds = new DecompressionStream(encoding);
+    const writer = ds.writable.getWriter();
+    writer.write(byteArray);
+    writer.close();
+    return new Response(ds.readable).text();
+}
+
+// save code to file
+function downloadCode() {
+    var text = window.theEditor.getValue();
+    var filename = "Main.elm";
+    var blob = new Blob([text], { type: "text/plain" });
+    var url = URL.createObjectURL(blob);
+    var a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function () {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }, 0);
+}
+
+// open code from file
+function uploadCode() {
+    var input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = e => {
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        reader.readAsText(file, 'UTF-8');
+        reader.onload = readerEvent => {
+            window.theEditor.setValue(readerEvent.target.result);
+        }
+    }
+    input.click();
+}
+
+// create url with compressed code and copy to clipboard
+function shareCode() {
+    compress(window.theEditor.getValue(), 'gzip').then(function (compressed) {
+        var url = window.location.href.split('?')[0] + '?compressed=' + encodeURIComponent(compressed);
+        navigator.clipboard.writeText(url).then(function () {
+            alert('Link copied to clipboard');
+        }, function (err) {
+            console.error('Could not copy text: ', err);
+        });
+    });
+}
+
+// drag and drop file to editor
+window.addEventListener('dragover', function (e) {
+    e.preventDefault();
+}, false);
+window.addEventListener('drop', function (e) {
+    e.preventDefault();
+    var reader = new FileReader();
+    reader.readAsText(e.dataTransfer.files[0], 'UTF-8');
+    reader.onload = readerEvent => {
+        window.theEditor.setValue(readerEvent.target.result);
+    }
+}, false);
+
+$(".panel-top").resizable({
+    handleSelector: ".splitter-horizontal",
+    resizeWidth: false,
+});
+$(".panel-bottom").resizable({
+    handleSelector: ".splitter-horizontal",
+    resizeWidth: false,
+});
+
+
+
+// save code to server
+function save() {
+    fetch('/save/' + window.repl_name + '?data=' + encodeURIComponent(window.theEditor.getValue()), { method: 'POST' });
+}
+
+// warn before reload/closing
+window.onbeforeunload = function () {
+    return 'saved your changes?';
+};
+
+// get url parameter ?code=... and set it as the editor content
+// get url parameter ?compressed=... and set it as the editor content
+function getEditorValue() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+    const compressed = urlParams.get('compressed');
+
+    if (code) {
+        fetch('/save/' + window.repl_name + '?data=' + encodeURIComponent('module Main exposing (..)\n\n' + code), { method: 'POST' });
+        value = 'module Main exposing (..)\n\n' + code;
+    } else if (compressed) {
+        decompress(compressed, 'gzip').then(function (decompressed) {
+            window.theEditor.setValue(decompressed);
+        });
+        value = "decompressing ..."
+    } else {
+        value = 'module Main exposing (..)\nnachricht = "Hello World"'
+    }
+    return value;
+}
+
+require.config({ paths: { 'vs': '/static/monaco-editor/min/vs' } });
+
+require(['vs/editor/editor.main'], function () {
+    monaco.languages.register({ id: 'Elm' });
+
+    monaco.languages.setMonarchTokensProvider('Elm', window.elm_monarch);
+
+    // monaco-editor theme & colors
+    monaco.editor.defineTheme('dark-plus', {
+        base: 'vs-dark',
+        inherit: true,
+        rules: [
+            { token: 'keyword', foreground: '#C586C0' },
+            { token: 'type', foreground: '#569CD6' },
+            { token: 'function.name', foreground: '#DCDCAA' },
+        ],
+        colors: {},
+    });
+
+    // init editor
+    var editor = monaco.editor.create(document.getElementById('container'), {
+        value: getEditorValue(),
+        language: 'Elm',
+        automaticLayout: true,
+        scrollBeyondLastLine: false,
+        theme: "dark-plus",
+        automaticLayout: true,
+    });
+
+    // auto-save on change
+    monaco?.editor?.getModels()[0].onDidChangeContent(save);
+    // save with ctrl + s in editor window
+    editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, save);
+
+    window.theEditor = editor;
+});
+
+// TERMINAL
+
+let term,
+    socketURL,
+    socket;
+const fit = new FitAddon.FitAddon();
+const terminalContainer = document.getElementById('terminal-container');
+
+const createTerminal = () => {
+    term = new Terminal({
+        cursorBlink: false
+    });
+
+    socketURL = (location.protocol === 'https:') ? 'wss://' : 'ws://' + location.hostname + ((location.port) ? (':' + location.port) : '') + '/ws/';
+
+    term.open(terminalContainer);
+
+    //  ctrl + c for copy when selecting data in terminal, default otherwise. ctrl + v for paste
+    term.attachCustomKeyEventHandler((arg) => {
+        if (arg.ctrlKey && arg.code === "KeyC" && arg.type === "keydown") {
+            const selection = term.getSelection();
+            if (selection) {
+                document.execCommand('copy')
+                term.clearSelection();
+                return false;
+            }
+        }
+        if (arg.ctrlKey && arg.code === "KeyV" && arg.type === "keydown") {
+            navigator.clipboard.readText()
+                .then(text => {
+                    socket.send(text)
+                })
+        };
+        return true;
+    });
+
+    // dynamic resize of terminal
+    term.loadAddon(fit);
+    fit.fit();
+    term.onResize(function (size) {
+        if (!window.repl_name) {
+            return;
+        }
+        fetch('/terminals/' + window.repl_name + '/size?cols=' + size.cols + '&rows=' + size.rows, { method: 'POST' });
+    });
+
+    // resize terminal on server
+    fetch('/terminals?cols=' + term.cols + '&rows=' + term.rows, { method: 'POST' }).then((res) => {
+        if (!res.ok) {
+            notify();
+            return;
+        }
+
+        res.text().then((repl_name) => {
+            window.repl_name = repl_name;
+            socketURL += repl_name;
+            socket = new WebSocket(socketURL);
+            socket.onopen = runRealTerminal;
+            socket.onclose = notify;
+            socket.onerror = notify;
+        });
+    }).catch((e) => {
+        document.getElementById("overlay").innerText = e;
+        notify();
+    });;
+}
+
+createTerminal();
+
+new ResizeObserver(function () { fit.fit(); }).observe(document.getElementsByClassName("panel-top")[0])
+
+const notify = () => {
+    terminalContainer.style.opacity = 0.5;
+    term.write("\r\nConnection lost.\r\n")
+}
+
+// connect terminal to websocket
+const runRealTerminal = () => {
+    term.loadAddon(new AttachAddon.AttachAddon(socket));
+    term._initialized = true;
+}
+
+// if parameter ?repl=... is set send to websocket
+const urlParams = new URLSearchParams(window.location.search);
+const repl = urlParams.get('repl');
+if (repl) {
+    setTimeout(() => {
+        socket.send("import Main exposing (..)\n" + repl + "\n")
+    }, 2000);
+} else {
+    setTimeout(() => {
+        socket.send("import Main exposing (..)\n")
+    }, 2000);
+}
